6b56ba4ba3812e28d5173683166ac8a46186f6f0
llleiron <sickboy23@mail.ru>
2020-11-17 00:44:23 +0400

    first commit

---
+++ tnayin1.py
@@ -0,0 +1,7 @@
+a = int(input())
+b = float(input())
+
+a = float(a)
+b = int(b)
+
+print(a + b, a - b, a / b, a * b, sep = " ")
---
+++ tnayin2.py
@@ -0,0 +1,5 @@
+import math
+
+x = float(input())
+
+print (math.pow(x, 2), math.pow(x, 3), math.pow(x, 5), math.pow(x, 10), sep = " ")
